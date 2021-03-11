969e0bbdfdfd8a796f1ddba540417789bf000f15
Erik Forsman <erik.jw.forsman@gmail.com>
2021-03-11 14:03:39 +0200

    listan käyttäjien hallintaa

--- src/components/EditListUsers.js
+++ src/components/EditListUsers.js
@@ -1,21 +1,57 @@
 import React from "react"
+import {gql, useQuery, useMutation} from "@apollo/client"
+import {FIND_LIST, ADD_USER_TO_LIST, REMOVE_USER_FROM_LIST} from "../queries"
 
 const EditListUsers = (props) => {
+  //Ota oma kysely ja kytke mutaatio tänne tai edelliseen
+  const [addUser] = useMutation(ADD_USER_TO_LIST,{
+    refetchQueries: [
+      {
+        query:FIND_LIST,
+        variables: {listId:props.pageProperties.id}
+      }
+    ]
+  })
+  const [removeUser] = useMutation(REMOVE_USER_FROM_LIST, {
+    refetchQueries: [
+      {
+        query:FIND_LIST,
+        variables: {listId:props.pageProperties.id}
+      }
+    ]
+  })
+
+
   console.log(props)
 
-  //omat kontaktit: {props.userContacts.map(u => u.username)} <br/>
-
-  //console.log(typeof(props.pageProperties.listMembers[0].username))
+  //Nykyiset käyttäjät miinus itse
   const vali = props.pageProperties.listMembers.filter(m => m.username !== props.user.username)
-  console.log(vali)
+  const chekki = vali.map(m => m.username)
+  //console.log("vali",vali)
+  //console.log("chekki", chekki)
+  //Käyttäjän kontaktit
+
+
   const canBeAdded = vali.filter(a => a.username )
+  //console.log(canBeAdded)
   const canBeRemoved = []
+
+  const sisaan=(props.userContacts)
+  // console.log(sisaan)
+  // const ass = sisaan.map(u => u.username)
+  // console.log("TÄSSÄ:",ass)
+  const voiLisata=sisaan.filter(u => !chekki.includes(u.username))
+  //console.log(voiLisata)
+
+//{props.userContacts.map(c => c.username)}<br/>
 
   return(
     <div>
     omat kontaktit: {props.userContacts.map(u => u.username)} <br/>
     nykyiset käyttäjät: {props.pageProperties.listMembers.map(m => m.username)}<br/>
-    Poista Listan käyttäjistä: <br/>{vali.map(v => <button key={v.id}>{v.username}</button>)}<br/>
+    Poista Listan käyttäjistä: <br/>{vali.map(v => <button key={v.id} onClick={()=>console.log("Poistettu")}>{v.username}</button>)}<br/>
+
+    Lisää listan käyttäjäksi: <br/>{voiLisata.map(c => <button key={c.id} onClick={()=>console.log("Lisätty")}>{c.username}</button>)}<br/>
 
       Täällä voit editoida käyttäjiä
       <button>Takaisin</button>
--- src/queries.js
+++ src/queries.js
@@ -83,6 +83,22 @@ export const ADD_SHOPPINGLIST = gql`
   }
 `
 
+export const ADD_USER_TO_LIST = gql`
+  mutation addListUser($listId:String!, $nameToAdd:String!){
+    addUserToList(listId:$listId, nameToAdd:$nameToAdd){
+      listName
+    }
+  }
+`
+
+export const REMOVE_USER_FROM_LIST = gql`
+  mutation removeListUser($listId:String!, $username:String!){
+    removeUserFromList(listId: $listId, username:$username){
+      listName
+    }
+  }
+`
+
 export const ADD_ITEM = gql`
   mutation addItem($listId: String!,$itemName: String!, $itemAmount:String, $itemNote:String){
     addItemToList(listId: $listId, itemName: $itemName, itemAmount: $itemAmount, itemNote:$itemNote){
